7a81557ef031960db9989db4430406104122b10d
dongxiaochen <dongxiaochen@adintl.cn>
2018-08-01 13:26:29 +0800

    up

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,6 +1,8 @@
 <!--  created by dongxc on 2018// -->
 <template>
-  <div>排行</div>
+  <div>
+    <img style="width:100%" src="http://139.224.21.205/materials/2018-07-19/be68c9360e344f3737d71b17a1345f1d.jpg" alt="">
+  </div>
 </template>
 
 <script type='text/ecmascript-6'>
