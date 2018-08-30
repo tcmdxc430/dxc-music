16e3ab07c637afb3c7c5860b477925c5477bed38
dongxiaochen <dongxiaochen@adintl.cn>
2018-08-30 11:03:00 +0800

    跳转歌手详情页的过渡动画

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,7 +1,10 @@
 <!--  created by dongxc on 2018/8/24 -->
 <template>
 <!-- 歌手详情页面 -->
-  <div class="singer-detail"></div>
+    <!-- 过度动画 -->
+    <transition name="slide">
+        <div class="singer-detail"></div>
+    </transition>
 </template>
 
 <script type='text/ecmascript-6'>
@@ -31,8 +34,13 @@ export default {
     position fixed 
     z-index 100
     top 0
-    bottom 0
+    bottom 0    
     left 0
     right 0
     background $color-background
+.slide-enter-active,.slide-leave-ative
+    transition all 0.3s
+.slide-enter,.slide-leave-to
+    // 元素从左向右偏移100%的初始位置
+    transform translate3d(100%,0,0)
 </style>
